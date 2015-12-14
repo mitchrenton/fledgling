2820bbfe5f3dbf7c91d196dec4a618556833ae94
Mitchell Renton <mitchrenton@gmail.com>
2015-12-14 12:00:54 +0100

    Say hello to Version 2

---
+++ grunt/sass.js
@@ -0,0 +1,10 @@
+module.exports = {
+	dist: {
+		options: {
+    		style: 'expanded'
+    	},
+		files: {
+			'public/css/fledgling.min.css' : 'local/scss/style.scss'
+		}
+	}
+};
---
+++ grunt/watch.js
@@ -0,0 +1,14 @@
+module.exports = {
+  css: {
+    tasks: ['sass','postcss','versioning:css'],
+    files: ['local/scss/**/*.scss']
+  },
+  plugins: {
+    tasks: ['jshint', 'concat:plugins', 'uglify:plugins','clean','versioning:plugins'],
+    files: ['local/js/plugins/*.js']
+  },
+  app: {
+    tasks: ['jshint', 'concat:app', 'uglify:app','clean','versioning:app'],
+    files: ['local/js/app/*.js']
+  }
+};
